e36717a1b94cc1b3b824b8dd0b74cbbbd7320839
almalki1234 <33692230+almalki1234@users.noreply.github.com>
2018-09-07 22:47:03 +0930

    update agent. now it is scaleable

--- src/tracks/singlePlayer/simple/sampleonesteplookahead/Agent.java
+++ src/tracks/singlePlayer/simple/sampleonesteplookahead/Agent.java
@@ -1,21 +1,30 @@
 package tracks.singlePlayer.simple.sampletwosteplookahead;
- import tracks.singlePlayer.tools.Heuristics.SimpleStateHeuristic;
+
+import tracks.singlePlayer.tools.Heuristics.SimpleStateHeuristic;
 import core.game.StateObservation;
 import core.player.AbstractPlayer;
 import ontology.Types;
 import tools.ElapsedCpuTimer;
 import tools.Utils;
- import java.util.Random;
- /**
+
+import java.util.Random;
+
+/**
  * 
  */
 public class Agent extends AbstractPlayer {
-     public double epsilon = 1e-6;
+
+    public double epsilon = 1e-6;
     public Random m_rnd;
-     public Agent(StateObservation stateObs, ElapsedCpuTimer elapsedTimer) {
-         m_rnd = new Random();
-     }
-     /**
+
+    public Agent(StateObservation stateObs, ElapsedCpuTimer elapsedTimer) {
+
+        m_rnd = new Random();
+
+
+    }
+
+    /**
      *
      * two step look ahead
      *
@@ -23,56 +32,45 @@ public class Agent extends AbstractPlayer {
      * @param elapsedTimer Timer when the action returned is due.
      * @return An action for the current state
      */
-    public Types.ACTIONS act(StateObservation stateObs, ElapsedCpuTimer elapsedTimer) {
-         Types.ACTIONS bestAction = null;
+    public Types.ACTIONS act(StateObservation stateObs, ElapsedCpuTimer elapsedTimer) 
+    {
+    	return act_inner(stateObs,elapsedTimer, 2);    	
+    }
+    
+    
+    public Types.ACTIONS act_inner(StateObservation stateObs,ElapsedCpuTimer elapsedTimer, int ntimes) 
+    {
+    	
+    		
+        Types.ACTIONS bestAction = null;
         double maxQ = Double.NEGATIVE_INFINITY;
         SimpleStateHeuristic heuristic =  new SimpleStateHeuristic(stateObs);
         for (Types.ACTIONS action : stateObs.getAvailableActions()) 
         {
-        	
+        	long remaining = elapsedTimer.remainingTimeMillis();
             StateObservation stCopy = stateObs.copy();
             // move according to searched action
             stCopy.advance(action);
             
-            // move 2nd time according to best action
-            stCopy.advance(act2(stCopy));
+            // move one more step in depth if we have enough time
+            if (ntimes>1 && remaining>50)
+            	stCopy.advance(act_inner(stCopy,elapsedTimer,ntimes-1));
                         
-            // measure 2 successive action's value
+            // measure ntimes successive action's value
             double Q = heuristic.evaluateState(stCopy);
             Q = Utils.noise(Q, this.epsilon, this.m_rnd.nextDouble());
-             //System.out.println("Action:" + action + " score:" + Q);
+
+            //System.out.println("Action:" + action + " score:" + Q);
             if (Q > maxQ) {
                 maxQ = Q;
                 bestAction = action;
             }
-         }
-         //System.out.println("======== "  + maxQ + " " + bestAction + "============");
+        }        
         return bestAction;
     }
     
     
-    private Types.ACTIONS act2(StateObservation stateObs) 
-    {
-         Types.ACTIONS bestAction = null;
-        double maxQ = Double.NEGATIVE_INFINITY;
-        SimpleStateHeuristic heuristic =  new SimpleStateHeuristic(stateObs);
-        for (Types.ACTIONS action : stateObs.getAvailableActions()) 
-        {
-        	
-            StateObservation stCopy = stateObs.copy();
-            stCopy.advance(action);
-            
-            
-            
-            double Q = heuristic.evaluateState(stCopy);
-            Q = Utils.noise(Q, this.epsilon, this.m_rnd.nextDouble());
-             //System.out.println("Action:" + action + " score:" + Q);
-            if (Q > maxQ) {
-                maxQ = Q;
-                bestAction = action;
-            }
-         }
-         //System.out.println("======== "  + maxQ + " " + bestAction + "============");
-        return bestAction;
-    }
- }
+    
+
+
+}
